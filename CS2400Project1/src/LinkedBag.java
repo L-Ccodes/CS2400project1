e64b9ea17a150660182ff808915dcb227d1dd294
Luis Navarrete <lanavarrete@cpp.edu>
2021-09-27 15:34:54 -0700

    Third commit

--- CS2400Project1/src/LinkedBag.java
+++ CS2400Project1/src/LinkedBag.java
@@ -1,15 +1,15 @@
-
 public class LinkedBag<T> implements BagInterface<T>{
-
+	
+	//Initialize private nodeOne and size
 	private Node nodeOne;
-	private int size;
+	private int size = 0;
 	
 	/** Constructor where a new linked bag is passed through
 	 */
-	public LinkedBag() {
+	public LinkedBag(int capacity) {
 	   nodeOne = null;
-	   size = 0;
-	}
+	   size = capacity;
+	}//end constructor
     
 	/** Gets the current number of entries that is in the bag.
 	 * @return returns the number of entries that is in the bag.
@@ -17,7 +17,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 	public int getCurrentSize() {
 		
 		return size;
-	}
+	}//end getCurrentSize
 
     /** Checks to see if the bag is empty.
      *  @return size = 0, if the bag is empty.
@@ -25,7 +25,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 	public boolean isEmpty() {
 		
 		return size == 0;
-	}
+	}//end isEmpty
 
 	/** Adds a new entry to the bag (node).
 	 * @param newEntry Is the new entry that is to be added.
@@ -38,28 +38,28 @@ public class LinkedBag<T> implements BagInterface<T>{
 		nodeOne = newNode;
 		size++;
 		return true;
-	}
+	}//end add
 
 	/** Removes a non specific entry from the bag
 	 *  @return remove when the entry was removed from the bag
 	 */
 	public T remove() {
-		
+		//removes random entry
 		T remove = null;
 		if(nodeOne != null) {
-		remove = nodeOne.getData();
-		nodeOne = nodeOne.getNextNode();
+			remove = nodeOne.getData();
+			nodeOne = nodeOne.getNextNode();
 		}	
 				
 		return remove;
-	}
+	}//end remove
 	
 	/** It gets the reference of the node that contains anEntry.
 	 * @param anEntry is the entry where the desired reference is.
 	 * @return the reference of anEntry.
 	 */
 	private Node getReferenceTo(T anEntry){
-		
+		//gets reference to an entry
 		boolean found = false;
 		Node currentNode = nodeOne;
 
@@ -79,6 +79,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 	 * @return remove is returned when the entry is removed from the bag.
 	 */
 	public boolean remove(T anEntry) {
+		//removes a specific entry
 		boolean remove = false;
 		Node desiredNode = getReferenceTo(anEntry);
 			
@@ -87,9 +88,9 @@ public class LinkedBag<T> implements BagInterface<T>{
 				nodeOne = nodeOne.getNextNode();
 				
 				size--;
-			}
+			}//end if
 		return remove;
-	}
+	}//end remove
 
 	/** Clears every node that is in the bag.
 	 */
@@ -97,7 +98,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 		
 		while(!isEmpty()) {
 			remove();
-		}
+		}//end while
 	}
 
 	/** Counts how many times a certain entry is within a bag.
@@ -106,7 +107,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 	 * @return the number of times the entry was found in the bag.
 	 */
 	public int getFrequencyOf(T anEntry) {
-		
+		//declare frequency and count
 		int frequency = 0;
 		int count = 0;
 		
@@ -114,8 +115,10 @@ public class LinkedBag<T> implements BagInterface<T>{
 		while((count < size) && currentNode != null) {
 			if(anEntry.equals(currentNode.getData())) {
 				frequency++;
-			}
-		}
+			}//end if
+			count++;
+			currentNode = currentNode.getNextNode();
+		}//end while
 		return frequency;
 	}
 
@@ -124,7 +127,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 	 * @return true if the entry is found.
 	 */
 	public boolean contains(T anEntry) {
-		
+		//checks if entry is contained
 		boolean contains = false;
 		Node currentNode = nodeOne;
 		
@@ -133,7 +136,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 				contains = true;
 			} else {
 				currentNode = currentNode.getNextNode();
-			}
+			}//end if else
 		}
 		return contains;
 	}
@@ -142,16 +145,17 @@ public class LinkedBag<T> implements BagInterface<T>{
 	 *  @return Returns the new array.
 	 */
 	public T[] toArray() {
-		
+		//converts to array
 		@SuppressWarnings("unchecked")
-		T[] array = (T[]) new ResizableArrayBag[size];
+		T[] array = (T[]) new Object[size];
+		
 		int i = 0;
 		Node currentNode = nodeOne;
 		while((i < size) && (currentNode != null)) {
 			array[i] = currentNode.getData();
 			i++;
 			currentNode = currentNode.getNextNode();
-		}
+		}//end while
 		return array;
 	}
 
@@ -163,17 +167,17 @@ public class LinkedBag<T> implements BagInterface<T>{
 	 */
 	public BagInterface<T> union(BagInterface<T> bag1, BagInterface<T> bag2) {
 		
-		int minCapacity = bag1.getCurrentSize() + bag2.getCurrentSize();
-		BagInterface<T> newBag = new ResizableArrayBag<T>(minCapacity);
+		//int minCapacity = bag1.getCurrentSize() + bag2.getCurrentSize();
+		BagInterface<T> newBag = new LinkedBag<>(0);
 		T[] array1 = bag1.toArray();
 		T[] array2 = bag2.toArray();
 		
 		for(int i = 0; i < bag1.getCurrentSize(); i++) {
 			newBag.add(array1[i]);
-		}
+		}//end for
 		for(int i = 0; i < bag2.getCurrentSize(); i++) {
-			newBag.add(array2[i]);
-		}
+ 			newBag.add(array2[i]);
+		}//end for
 		
 		return newBag;
 	}
@@ -185,8 +189,8 @@ public class LinkedBag<T> implements BagInterface<T>{
 	 */
 	public BagInterface<T> intersection(BagInterface<T> bag1, BagInterface<T> bag2) {
 		
-		int minCapacity = Math.min(bag1.getCurrentSize(), bag2.getCurrentSize()) + 1;
-		BagInterface<T> newBag = new ResizableArrayBag<T>(minCapacity);
+		//int minCapacity = Math.min(bag1.getCurrentSize(), bag2.getCurrentSize()) + 1;
+		BagInterface<T> newBag = new LinkedBag<>(0);
 		
 		T[] array = bag1.toArray();
 		
@@ -198,11 +202,11 @@ public class LinkedBag<T> implements BagInterface<T>{
 				int second = bag2.getFrequencyOf(array[i]);
 				int minNum = Math.min(first, second);
 				
-				for(int j = 0; j < minNum; i++) {
-					newBag.add(array[i]);
-				}
-			}
-		}
+				for(int j = 0; j < minNum; j++) {
+					newBag.add(array[j]);
+				}//end for
+			}//end if else
+		}//end for
 		return newBag;
 	}
 
@@ -216,7 +220,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 		
 		int returnObject;
 		int minCapacity = Math.max(bag1.getCurrentSize(), bag2.getCurrentSize());
-		BagInterface<T>	newBag = new ResizableArrayBag<T>(minCapacity);
+		BagInterface<T>	newBag = new LinkedBag<>(0);
 		
 		T[] array = bag1.toArray();
 		
@@ -232,22 +236,16 @@ public class LinkedBag<T> implements BagInterface<T>{
 				returnObject = second - first;
 				for(int j = 0; j < returnObject; j++) {
 					newBag.add(array[i]);
-				}
-			}
-		}
+				}//end for
+			}//end if else
+		}//end for
 			
 		
 		return newBag;
 	}
 	
-	
-	
-	
-	
-	
-	
 	private class Node{
-		
+		//Initialize data and next
 		private T    data; // Entry in bag
 		private Node next;
 		
@@ -256,7 +254,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 		 */
 		private Node(T data){
 			this(data, null);
-		}
+		}//end constructor
 		/** A constructor that creates a new node.
 		 * @param data is what will be inside the array.
 		 * @param nextNode is the reference to the next node.
@@ -265,21 +263,21 @@ public class LinkedBag<T> implements BagInterface<T>{
 			
 			this.data = data;
 			next = nextNode;
-		}
+		}//end constructor
 		
 		/** Used to get what is inside of a node.
 		 * @return the data inside the node.
 		 */
 		private T getData() {
 			return data;
-		}
+		}//end getData
 		
 		/** This is used to enter data into a node.
 		 * @param newData is what will be entered into the node.
 		 */
 		private void setData(T newData) {
 			data = newData;
-		}
+		}//end setData
 		
 		/** Used to refer to the next node.
 		 * @return the next node.
@@ -287,7 +285,7 @@ public class LinkedBag<T> implements BagInterface<T>{
 		private Node getNextNode() {
 			
 			return next;
-		}
+		}//end getNextNode
 
 	}
 
