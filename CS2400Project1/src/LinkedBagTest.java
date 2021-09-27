7f24f5385efe9d1f722c36f8baa07806fdcea670
Christian Williams <ciwilliams@cpp.edu>
2021-09-27 15:48:17 -0700

    added javadoc

--- CS2400Project1/src/LinkedBagTest.java
+++ CS2400Project1/src/LinkedBagTest.java
@@ -1,11 +1,14 @@
 import java.util.Arrays;
 
-public class LinkedBagTest {
+public class LinkedBagTest { //LinkedBagTest class
 
+	/** Main method to test the three methods.
+	 * @param args default parameter.
+	 */
 	public static void main(String[] args) {
 		//Initializing the 2 bags that will be used
-		BagInterface <String> bag1 = new LinkedBag < >();
-		BagInterface <String> bag2 = new LinkedBag < >();
+		BagInterface <String> bag1 = new LinkedBag < >(0);
+		BagInterface <String> bag2 = new LinkedBag < >(0);
 
 		//Adding each of the letters to bag1
 		bag1.add("A");
